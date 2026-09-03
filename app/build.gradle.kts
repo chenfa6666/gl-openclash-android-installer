1c6647c36bffbd2ffed61521081030ce3efde124
chen <chen@DESKTOP-VMTB6FV>
2026-09-03 15:01:49 +0800

    TEST-D

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,7 +87,6 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
